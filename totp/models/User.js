a5269ffd9439844a38885a44172a35e4f146f87b
Thania Cisneros <cisnerth@oregonstate.edu>
2026-04-27 14:56:10 -0500

    remove totp User model and authController

--- totp/models/User.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-  mfaSecret: String
-});
-
-module.exports = mongoose.model("User", userSchema);
